e7027dbdc853931ad085b8c0156c248e73eacd04
Ilayda Ozbay <ilaydaozbay4@gmail.com>
2025-01-18 20:55:25 +0300

    CartTest completed

---
+++ com.lcwautomation/src/test/java/tests/CartTest.java
@@ -0,0 +1,47 @@
+package tests;
+
+import org.testng.annotations.Test;
+import pages.*;
+
+public class CartTest extends BaseTest{
+
+    HomePage homepage;
+    LoginPage loginpage;
+    CategoryPage categorypage;
+    FilterPage filterpage;
+    SortPage sortpage;
+    ProductPage productpage;
+    CartPage cartpage;
+
+    @Test
+    public void addToCartTest() throws InterruptedException {
+
+        homepage = new HomePage(driver);
+        loginpage = new LoginPage(driver);
+        categorypage = new CategoryPage(driver);
+        filterpage = new FilterPage(driver);
+        sortpage = new SortPage(driver);
+        productpage = new ProductPage(driver);
+        cartpage = new CartPage(driver);
+
+        homepage.goToLoginPage().logintoLCW("ilaydaozbaay@gmail.com", "LCW_Test_Automation1");
+
+        categorypage.selectCategoryType("Çocuk&Bebek","Kız Çocuk (6-14 YAŞ)","Mont ve Kaban");
+
+        filterpage.selectFilters();
+
+        sortpage.sortby();
+
+        productpage.selectProduct()
+                   .closeTransparentOverlay()
+                   .addToCart();
+
+        cartpage.navigateToCart()
+                .verifyProductName(productpage.detailName)
+                .verifyProductColor(productpage.detailColor)
+                .verifyProductQuantity(productpage.detailQuantity)
+                .verifyProductPrice()
+                .addProduct()
+                .removeProduct();
+    }
+}
